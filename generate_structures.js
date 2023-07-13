5d00d321309e96efbfdaa1384dab89f71694ebd7
Filmos <filmosyt@gmail.com>
2023-07-13 18:26:11 +0200

    Added floor patterns to buildings

--- generate_structures.js
+++ generate_structures.js
@@ -8,6 +8,33 @@ class Building {
         this.floorId = floorId
         this.structure = []
     }
+
+    clone(idSuffix) {
+        let building = new Building(this.buildingId + idSuffix, this.floorId)
+        building.structure = JSON.parse(JSON.stringify(this.structure))
+        return building
+    }
+
+    fill_slice(slice, char) {
+        slice = this.structure[slice]
+        for(let i=0;i<slice.length;i++)
+            slice[i] = slice[i].replaceAll("?", char)
+    }
+
+    add_to_slice(slice, pattern, override=false) {
+        slice = this.structure[slice]
+        for(let i=0;i<slice.length;i++) {
+            let newPattern = ""
+            for(let j=0;j<slice[i].length;j++) {
+                if(slice[i][j] == "?" || override)
+                    newPattern += pattern[i][j]
+                else
+                    newPattern += slice[i][j]
+            }
+            slice[i] = newPattern
+        }
+    }
+
 }
 
 function load_walls() {
@@ -21,6 +48,25 @@ function load_walls() {
         walls.push(wall)
     }
     return walls
+}
+
+function load_floor_patterns(buildingBases) {
+    let patterns = {}
+    for (let file of fs.readdirSync('src/structure/floors'))
+        patterns[file.split('.')[0]] = JSON.parse(fs.readFileSync('src/structure/floors/' + file))
+    
+        let buildings = []
+    for(let building of buildingBases) {
+        for(let p in patterns) {
+            let newBuilding = building.clone(p)
+            newBuilding.add_to_slice(0, patterns[p])
+            newBuilding.fill_slice(4, "#")
+            newBuilding.fill_slice(5, "#")
+            buildings.push(newBuilding)
+        }
+    }
+    
+    return buildings
 }
 
 function generate_floors(building) {
@@ -45,6 +91,7 @@ function save_building(building, floors) {
 
 function generate_structures() {
     let buildings = load_walls()
+    buildings = load_floor_patterns(buildings)
 
     for(let building of buildings)
         save_building(building, generate_floors(building))
